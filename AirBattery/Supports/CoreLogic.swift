14d3a40b96c49ca603e0002edb64a5835dac6108
Joseph Courtney <5942706+josephcourtney@users.noreply.github.com>
2026-09-20 08:58:10 -0400

    fix: model stable cross-source device identifiers [skip ci]

--- AirBattery/Supports/CoreLogic.swift
+++ AirBattery/Supports/CoreLogic.swift
@@ -1,5 +1,43 @@
 import Foundation
 
+enum DeviceObservationSource: String, Codable, Equatable {
+    case ble
+    case libimobiledevice
+}
+
+struct DeviceIdentifierSet: Equatable {
+    var canonicalID: String
+    var mobileDeviceID: String?
+    var bleDeviceID: String?
+
+    mutating func merge(
+        canonicalID incomingCanonicalID: String,
+        mobileDeviceID incomingMobileDeviceID: String?,
+        bleDeviceID incomingBLEDeviceID: String?
+    ) {
+        if let incomingMobileDeviceID, !incomingMobileDeviceID.isEmpty {
+            mobileDeviceID = incomingMobileDeviceID
+        }
+        if let incomingBLEDeviceID, !incomingBLEDeviceID.isEmpty {
+            bleDeviceID = incomingBLEDeviceID
+        }
+
+        if let mobileDeviceID, !mobileDeviceID.isEmpty {
+            canonicalID = mobileDeviceID
+        } else if let bleDeviceID, !bleDeviceID.isEmpty {
+            canonicalID = bleDeviceID
+        } else {
+            canonicalID = incomingCanonicalID
+        }
+    }
+
+    func matches(_ identifier: String) -> Bool {
+        canonicalID == identifier ||
+            mobileDeviceID == identifier ||
+            bleDeviceID == identifier
+    }
+}
+
 enum IDeviceConnectionSource: String, Hashable {
     case network = "Network"
     case usb = "USB"
