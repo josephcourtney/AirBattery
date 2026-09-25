71a9e3f44e4f6300297e07190e206f742c0c2974
Joseph Courtney <5942706+josephcourtney@users.noreply.github.com>
2026-09-25 07:48:00 -0400

    Expose battery alert rule within package

--- Sources/AirBatteryShared/Domain/BatteryAlertRule.swift
+++ Sources/AirBatteryShared/Domain/BatteryAlertRule.swift
@@ -1,11 +1,29 @@
 import Foundation
 
-struct btAlert: Codable, Equatable {
-    let name: String
-    let full: Int
-    let fullOn: Bool
-    let fullSound: Bool
-    let low: Int
-    let lowOn: Bool
-    let lowSound: Bool
+package struct btAlert: Codable, Equatable {
+    package let name: String
+    package let full: Int
+    package let fullOn: Bool
+    package let fullSound: Bool
+    package let low: Int
+    package let lowOn: Bool
+    package let lowSound: Bool
+
+    package init(
+        name: String,
+        full: Int,
+        fullOn: Bool,
+        fullSound: Bool,
+        low: Int,
+        lowOn: Bool,
+        lowSound: Bool
+    ) {
+        self.name = name
+        self.full = full
+        self.fullOn = fullOn
+        self.fullSound = fullSound
+        self.low = low
+        self.lowOn = lowOn
+        self.lowSound = lowSound
+    }
 }
